fdfd407528292e294cf489fbcd80d341b4842c7c
sohyeon Park <qkrthgus7026.gmail.com>
2023-12-21 15:41:42 +0900

    booktable변경

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const port = 3010
 
 const db = mysql.createConnection({
 	host: 'svc.sel4.cloudtype.app',
-    port: 30579,
+  port: 30579,
 	user: 'root',
 	password: 'mysql1234',
-	database: 'songdb',
+	database: 'bookdb',
 })
 
 db.connect()
@@ -18,8 +18,8 @@ app.get('/', (req, res) => {
   res.json({result: '스마트앱프로그래밍 백엔드'})
 })
 
-app.get('/song', (req, res) => {
-    const sql = 'select * from song'
+app.get('/book', (req, res) => {
+    const sql = 'select * from Book'
     
       db.query(sql, (err, rows) => {
           if (err) {
